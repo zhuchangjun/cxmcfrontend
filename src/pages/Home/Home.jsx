b60507cf5123126e32365abfce2081550a61ce79
zhuchangjun <zhuchangjun@hotmail.com>
2024-12-27 01:45:52 -0500

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../components/AppDownload/AppDownload'
+
+const Home = () => {
+
+  const [category,setCategory] = useState("All")
+
+  return (
+    <>
+      <FoodDisplay category={category}/>
+      <AppDownload/>
+    </>
+  )
+}
+
+export default Home
